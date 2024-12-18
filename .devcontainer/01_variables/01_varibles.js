34cc66a549983cd49f331ea50a20a79372d0c973
codebyvivoo9595 <vivoo.thecoder@gmail.com>
2024-12-18 18:31:05 +0000

    variables in javascript

---
+++ .devcontainer/01_variables/01_varibles.js
@@ -0,0 +1,22 @@
+const ifsc = "BOI1223"
+let acno = 123456
+var data = "Getting message"
+variable1 = "12345566"
+
+//ifsc = "BOI1223"  we are using const so can not remodify
+
+//let is using within the scope 
+
+// var should not be use 
+
+//we can declare the variable like this but this is the wrong practice 
+//variable1 = "12345566"
+
+console.log(ifsc);
+console.table([ifsc,data,acno])
+// we can show all the data in tabular format 
+
+let rowcnt ;
+
+//then here rowcnt value is undefine
+// if we are not assigning any thing then the value is undefine.
