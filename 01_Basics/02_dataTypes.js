d20b5d09ea0df0ed88cb8fd0a403d3862267fcf4
codebyvivoo9595 <vivoo.thecoder@gmail.com>
2024-12-18 18:52:01 +0000

    02_dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"// treat all the code as the newer version of javascript
+
+//alert(3+3) 
+// we are using node js not browser
+
+// console.log(3+
+//     3
+// )
+//Code readability should be high (good)
+
+// Documentation 
+//tc39
+//mdn web
+
+// Datatypes 
+ let name = "Chaitalee"
+ let age = 25
+ let is_she_breave = true
+
+ //datatypes we used 
+ //number => 2 to power 53
+ //bigInt 
+ //string => "" or ''
+ // boolean => true or false 
+ // null (representation of empty)(standalone value)
+ // undefine (ethe apan assign kelele nahi kahi)
+ //symbol 
+
+ //object 
+
+ console.log(typeof age);
+
+ console.log(typeof undefined); //undefined (this one is type)
+ 
+ console.log(typeof null); // object (null is object in Javascript)
