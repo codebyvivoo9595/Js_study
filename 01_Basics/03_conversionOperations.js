0c688d303c6a67649ae4e952c24c18a550f6097e
Vivek Bagane <vivoo.thecoder@gmail.com>
2024-12-19 03:23:09 +0000

    conversion newly added

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -67,7 +67,49 @@
 //"Vivek"=>true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber) //33
-console.log(typeof(stringNumber))//String 
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) //33
+// console.log(typeof(stringNumber))//String 
+
+
+
+//************************Operations**************//
+let value = 3 
+
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2) // Addition
+// console.log(2-2) // Substraction
+// console.log(2*2) // Multiplication
+// console.log(2/2) // Divide
+// console.log(2%2) //Reminder
+// console.log(2**2) //power of 2
+// console.log(2**3) //power of 3
+
+//Concatination
+// let name1 = "Vivek "
+// let name2 = "& Chaitu"
+
+// console.log(name1+name2)
+
+// console.log("1"+2);//12 
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+//Rule: String jar pahila asel tr tyach concatination hot 
+//Rule: Jar string last la asel ani pahila kahi operation astil tr pahila operation perform hotat ani mg string concatination hote 
+
+// console.log(true)//True
+// console.log(+true)//1
+// console.log(true+);//Expression expected.
+// console.log(+"");// Value 0 yeil
+
+// let num1,num2,num3;
+// num1 = num2 =num3=2+2;
+//Code readabilty should be better not follow this practice
+
+   
+
