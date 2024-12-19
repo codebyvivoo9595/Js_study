5a6e3be037eaf1c0123c07406d3c6a9540442cbb
Vivek Bagane <vivoo.thecoder@gmail.com>
2024-12-19 02:42:05 +0000

    Conversion Changes

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,53 +1,53 @@
-let score = "33"
-console.log(typeof score); //one way of using
-console.log(typeof (score)); //second way of using
+//let score = "33"
+//console.log(typeof score); //one way of using
+//console.log(typeof (score)); //second way of using
 
 
-//Case : NaN
-let score1="333vivek"
+//-----Case : NaN-----
+//let score1="333vivek"
 //Coverting it into number 
-console.log(typeof(score1))
+//console.log(typeof(score1))
 
-let valueIsNumber = Number(score1)
-console.log(typeof(valueIsNumber)) 
+//let valueIsNumber = Number(score1)
+//console.log(typeof(valueIsNumber)) 
 //Data type is showing a number but when we put the value then we are getting NaN 9(Not a Number))  
 
-//Case : null
+//----Case : null-----
 
-let v1 = null
+//let v1 = null
 
-console.log(typeof(v1))//Object
-let checkIsNumber = Number(v1)
-console.log(typeof (checkIsNumber))//Number
-console.log(checkIsNumber)//0
+// console.log(typeof(v1))//Object
+// let checkIsNumber = Number(v1)
+// console.log(typeof (checkIsNumber))//Number
+// console.log(checkIsNumber)//0
 
-//Case : Undefined  
-let v2 = Undefined
+//-----Case : Undefined ----- 
+// let v2 = undefined
 
-console.log(typeof(v2))//Undefine
-let checkIsNumber2 = Number(v2)
-console.log(typeof (checkIsNumber2))//Number
-console.log(checkIsNumber2)//NaN
-
-
-//Case: boolean
-
-let checkval = true
-
-console.log(typeof(checkval))//boolean
-let checkIsNumber3 = Number(v1)
-console.log(typeof (checkIsNumber3))//Number
-console.log(checkIsNumber3)//True 1 false 0
+// console.log(typeof(v2))//Undefine
+// let checkIsNumber2 = Number(v2)
+// console.log(typeof (checkIsNumber2))//Number
+// console.log(checkIsNumber2)//NaN
 
 
-//Case : String
+//-----Case: boolean-----
 
-let stringval = "Chaitalee Madam"
+// let checkval = true
 
-console.log(typeof(stringval))//string
-let checkIsNumber4 = Number(stringval)
-console.log(typeof (checkIsNumber3))//Number
-console.log(checkIsNumber3)//NaN
+// console.log(typeof(checkval))//boolean
+// let checkIsNumber3 = Number(v1)
+// console.log(typeof (checkIsNumber3))//Number
+// console.log(checkIsNumber3)//True 1 false 0
+
+
+//-----Case : String-----
+
+// let stringval = "Chaitalee Madam"
+
+// console.log(typeof(stringval))//string
+// let checkIsNumber4 = Number(stringval)
+// console.log(typeof (checkIsNumber3))//Number
+// console.log(checkIsNumber3)//NaN
 
 //"33"=>33
 //"33abc"=>NaN
@@ -55,10 +55,11 @@ console.log(checkIsNumber3)//NaN
 
 //let isLoggedIn=1
 //let isLoggedIn = ""   //false
-let isLoggedIn = "Vivek" //True
 
-let booleanIsLogged = Boolean(isLoggedIn) //boolean
-console.log(booleanIsLogged)//True
+// let isLoggedIn = "Vivek" //True
+
+// let booleanIsLogged = Boolean(isLoggedIn) //boolean
+// console.log(booleanIsLogged)//True
 
 
 //1=>true
@@ -67,6 +68,6 @@ console.log(booleanIsLogged)//True
 
 
 let someNumber = 33
-let stringNumber = string(someNumber)
+let stringNumber = String(someNumber)
 console.log(stringNumber) //33
 console.log(typeof(stringNumber))//String 
