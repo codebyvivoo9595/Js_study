780d56dcf7fbe82def40b84262685c005b50f02c
Vivek Bagane <vivoo.thecoder@gmail.com>
2024-12-27 03:16:31 +0000

    #basic function

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,30 @@
+
+//------------Functions--------------- 
+
+//console.log('Hi vivek');
+
+// function sayHi(){
+//     console.log('Hi vivek & Chaitu');
+// }
+
+//sayHi - Referance
+//() - Execution 
+//sayHi()
+
+//                        parameters 
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+
+//          Argumnets
+//addTwoNumbers(10,15);
+
+//const result = addTwoNumbers(10,15);
+//console.log(result);//undefined we are not returning any thing 
+
+// function addTwoNumbers(number1,number2){
+//     let res =number1+number2 ;
+//     return res
+// }
+
+
