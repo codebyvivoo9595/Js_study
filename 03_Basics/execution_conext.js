13541afb9497ce3a36a24556c2a48344eb019b1d
Vivek Bagane <vivoo.thecoder@gmail.com>
2024-12-31 03:44:05 +0000

    #execution_context 0.0

---
+++ 03_Basics/execution_conext.js
@@ -0,0 +1,21 @@
+//javascript execution contect 
+/*
+//when ever execution start 
+//Global execution context ==>{}[this]
+//Javascrpit is singl threaded language
+total 3 execution context 
+1.Global execution context 
+2.Function/functional execution context 
+3.Eval Execution context 
+
+[{}] => Memory Creation phase / Creation phase
+ //here memory is allocate for variable but execution or value not assign here
+ 
+ let val1 = 10;
+ let val2 = 5
+
+ function add(num1,num2){return num1+num2}
+
+ let result1 = add(val1+val2)
+ let result2 = add(10,2)
+*/
