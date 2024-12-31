ee2cba17b2136c6202478536fd50e7f1ad612909
Vivek Bagane <vivoo.thecoder@gmail.com>
2024-12-31 13:20:23 +0000

    #execution_context

--- 03_Basics/execution_conext.js
+++ 03_Basics/execution_conext.js
@@ -18,4 +18,25 @@ total 3 execution context
 
  let result1 = add(val1+val2)
  let result2 = add(10,2)
+
+ then how the flow is going on let see
+
+ 1. Global Execution {} this
+ 2. Memory Phase (assign memory space not value )
+    //1)First cycle
+    val1= undefine
+    val2 = undefine
+    add = defination 
+
+    result1 = undefine
+    result2 = undefine
+  
+    //2) execution phase 
+    val1<=10
+    val2<=5
+    add==> New execution context (New execution Envoirment + Execution thread)
+    Note after executions of the function call the execution context will delete.
+    
+    (New execution Envoirment + Execution thread)
+  //   Memory Phase           + execution phase  
 */
